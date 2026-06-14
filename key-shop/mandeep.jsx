9af968829b341d06fa00eb36d952d9343eae86f1
kaurmandeep1202 <kaurmandeep272002@gmail.com>
2026-06-14 17:44:48 +0530

    key-shop

---
+++ key-shop/mandeep.jsx
@@ -0,0 +1,18 @@
+ //delete the entire code 
+
+ function App() {
+  return (
+     <div className="App">
+      <h1>Welcome to Key Shop</h1>
+      <p>This app is just for demonstration purposes.</p>
+      <button> click me</button>
+    </div>
+      )
+}
+
+//.jsx file is used to write html code in javascript file
+//returns html code in javascript file
+
+export default App;
+
+//go to main.jsx and import this App component and render it in the root element of the html file.
---
+++ key-shop/src/App.jsx
@@ -0,0 +1,19 @@
+ //delete the entire code 
+
+//import header from './header.jsx'
+import header from './components/header.jsx'
+
+ function App() {
+  return (
+     <div className="App">
+      <h1>Welcome to Key Shop</h1>
+      <p>This app is just for demonstration purposes.</p>
+      <button> click me</button>
+    </div>
+      )
+}
+
+//.jsx file is used to write html code in javascript file
+//returns html code in javascript file
+
+export default App;
