d5acf414c06f0c8d13f8e0d0cc8314e7cd0b2ddc
kaurmandeep1202 <kaurmandeep272002@gmail.com>
2026-06-14 17:44:27 +0530

    details.js

---
+++ key-shop/src/components/header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <h1>Key Shop</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/products">Products</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
